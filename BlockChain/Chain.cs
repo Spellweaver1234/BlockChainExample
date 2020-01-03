6d6973c9896bfd73a7ff93d67a1bda9ff89b10e6
Zorin Arseniy <Arsenchozzz@mail.ru>
2020-01-03 17:26:18 +0400

    Добавьте файлы проекта.

---
+++ BlockChain/Chain.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlockChain
+{
+    class Chain
+    {
+        public double amount;
+        public string hash;
+        public string prevHash;
+        public int index;
+        static public int count = 0;
+
+        public string getHash()
+        {
+            MD5 md5Hash = MD5.Create();
+            return GetMd5Hash(md5Hash, amount.ToString() + prevHash.ToString() + index.ToString());
+        }
+
+        public void CreateBlock(double a,string ph)
+        {
+            amount = a;
+            prevHash = ph;
+            index = count;
+            count++;
+            hash = getHash();
+        }
+
+        static string GetMd5Hash(MD5 md5Hash, string input)
+        {
+
+            // Convert the input string to a byte array and compute the hash.
+            byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+            // Create a new Stringbuilder to collect the bytes
+            // and create a string.
+            StringBuilder sBuilder = new StringBuilder();
+
+            // Loop through each byte of the hashed data 
+            // and format each one as a hexadecimal string.
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+
+            // Return the hexadecimal string.
+            return sBuilder.ToString();
+        }
+    }
+}
---
+++ BlockChain/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlockChain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Chain[] blockchain;
+            blockchain = new Chain[10];
+            for (int i = 0; i < blockchain.Length; i++)
+            {
+                blockchain[i] = new Chain();
+            }
+
+            blockchain[0].CreateBlock(100, "");
+            blockchain[1].CreateBlock(50, blockchain[0].hash);
+            blockchain[2].CreateBlock(250, blockchain[1].hash);
+            blockchain[3].CreateBlock(75, blockchain[2].hash);
+            blockchain[4].CreateBlock(250, blockchain[3].hash);
+            blockchain[5].CreateBlock(200, blockchain[4].hash);
+            blockchain[6].CreateBlock(100, blockchain[5].hash);
+            blockchain[7].CreateBlock(220, blockchain[6].hash);
+            blockchain[8].CreateBlock(30, blockchain[7].hash);
+
+            if (VerifyBC(blockchain))
+                Console.WriteLine("Блокчейн не нарушен");
+            else
+                Console.WriteLine("Блокчейн нарушен");
+
+            Console.ReadLine();
+        }
+
+        static bool VerifyBC(Chain[] blockchain)
+        {
+            for (int i = 1; i < Chain.count; i++)
+            {
+                if (blockchain[i].hash != blockchain[i].getHash())
+                {
+                    return false;
+                }
+
+                if (blockchain[i].prevHash != blockchain[i - 1].hash)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
